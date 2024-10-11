9fdb9c24b6585b8cc3075275d58962381d27c956
avdivo <avdivo@mail.ru>
2024-10-11 19:40:17 +0300

    В новой ветке альтернативный вариант views

--- movies/urls.py
+++ movies/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from .views import FilmListView, FilmDetailView, FilmCreateView, FilmUpdateView, FilmDeleteView
+from .views import (
+    film_list_view,
+    film_detail_view,
+    film_create_view,
+    film_update_view,
+    film_delete_view
+)
 
 urlpatterns = [
-    path('', FilmListView.as_view(), name='film-list'),  # Главная страница
-    path('films/<int:pk>/', FilmDetailView.as_view(), name='film-detail'),  # Страница деталей фильма
-    path('films/add/', FilmCreateView.as_view(), name='film-add'),  # Страница добавления фильма
-    path('films/<int:pk>/edit/', FilmUpdateView.as_view(), name='film-edit'),  # Страница редактирования фильма
-    path('films/<int:pk>/delete/', FilmDeleteView.as_view(), name='film-delete'),  # Страница удаления фильма
+    path('', film_list_view, name='film-list'),  # Главная страница - список фильмов
+    path('films/<int:pk>/', film_detail_view, name='film-detail'),  # Страница деталей фильма
+    path('films/add/', film_create_view, name='film-add'),  # Страница добавления фильма
+    path('films/<int:pk>/edit/', film_update_view, name='film-edit'),  # Страница редактирования фильма
+    path('films/<int:pk>/delete/', film_delete_view, name='film-delete'),  # Страница удаления фильма
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,41 +1,88 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
-from .models import Film
-from .forms import FilmForm
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .serializers import FilmSerializer
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Film
+from .forms import FilmForm
 
-# Вьюха для API
+
+# Вьюха для отображения списка фильмов
+def film_list_view(request):
+    """
+    Отображает список всех фильмов.
+    """
+    films = Film.objects.all()  # Получаем все фильмы из базы данных
+    context = {
+        'films': films  # Передаем фильмы в контекст
+    }
+    return render(request, 'movies/films_list.html', context)  # Рендерим шаблон
+
+# Вьюха для отображения деталей фильма
+def film_detail_view(request, pk):
+    """
+    Отображает детали конкретного фильма по его ID.
+    """
+    film = get_object_or_404(Film, pk=pk)  # Получаем фильм по ID или 404
+    context = {
+        'film': film  # Передаем фильм в контекст
+    }
+    return render(request, 'movies/film_detail.html', context)  # Рендерим шаблон
+
+# Вьюха для создания нового фильма
+def film_create_view(request):
+    """
+    Обрабатывает создание нового фильма.
+    """
+    if request.method == 'POST':  # Если метод POST, значит отправили форму
+        form = FilmForm(request.POST, request.FILES)  # Создаем форму с данными
+        if form.is_valid():  # Проверяем валидность формы
+            form.save()  # Сохраняем новый фильм в базу данных
+            return redirect('film-list')  # Перенаправляем на список фильмов
+    else:
+        form = FilmForm()  # Создаем пустую форму для GET-запроса
+
+    context = {
+        'form': form  # Передаем форму в контекст
+    }
+    return render(request, 'movies/film_form.html', context)  # Рендерим шаблон
+
+# Вьюха для редактирования фильма
+def film_update_view(request, pk):
+    """
+    Обрабатывает редактирование существующего фильма.
+    """
+    film = get_object_or_404(Film, pk=pk)  # Получаем фильм по ID
+    if request.method == 'POST':  # Если метод POST, значит отправили форму
+        form = FilmForm(request.POST, request.FILES, instance=film)  # Создаем форму с данными
+        if form.is_valid():  # Проверяем валидность формы
+            form.save()  # Сохраняем изменения в базе данных
+            return redirect('film-list')  # Перенаправляем на список фильмов
+    else:
+        form = FilmForm(instance=film)  # Создаем форму с текущими данными фильма
+
+    context = {
+        'form': form,  # Передаем форму в контекст
+        'film': film  # Передаем фильм в контекст
+    }
+    return render(request, 'movies/film_form.html', context)  # Рендерим шаблон
+
+# Вьюха для удаления фильма
+def film_delete_view(request, pk):
+    """
+    Обрабатывает удаление существующего фильма.
+    """
+    film = get_object_or_404(Film, pk=pk)  # Получаем фильм по ID
+    if request.method == 'POST':  # Если метод POST, значит пользователь подтвердил удаление
+        film.delete()  # Удаляем фильм
+        return redirect('film-list')  # Перенаправляем на список фильмов
+
+    context = {
+        'film': film  # Передаем фильм в контекст
+    }
+    return render(request, 'movies/film_confirm_delete.html', context)  # Рендерим шаблон
+
+# Вьюха для API (остается без изменений)
 class FilmViewSet(viewsets.ModelViewSet):
     queryset = Film.objects.all()
     serializer_class = FilmSerializer
     # permission_classes = [IsAuthenticatedOrReadOnly]
-
-# Вьюхи для веб-приложения остаются без изменений
-class FilmListView(ListView):
-    model = Film
-    template_name = 'movies/films_list.html'
-    context_object_name = 'films'
-
-class FilmDetailView(DetailView):
-    model = Film
-    template_name = 'movies/film_detail.html'
-
-class FilmCreateView(CreateView):
-    model = Film
-    form_class = FilmForm
-    template_name = 'movies/film_form.html'
-    success_url = reverse_lazy('film-list')
-
-class FilmUpdateView(UpdateView):
-    model = Film
-    form_class = FilmForm
-    template_name = 'movies/film_form.html'
-    success_url = reverse_lazy('film-list')
-
-class FilmDeleteView(DeleteView):
-    model = Film
-    template_name = 'movies/film_confirm_delete.html'
-    success_url = reverse_lazy('film-list')
